cdb9c88d26bb203a921149f68604de83e99e973f
K-IWASA-MK <205480127+K-IWASA-MK@users.noreply.github.com>
2026-09-15 20:50:14 +0900

    chore(config): sync data/config.js with deployment.json for KAMEYAMA

--- data/config.js
+++ data/config.js
@@ -3,14 +3,14 @@ window.PMS_CLIENT_CONFIG = {
   status: "ACTIVE_DEVELOPMENT",
   environment: "production",
   api: {
-    gasWebAppUrl: ""
+    gasWebAppUrl: "https://script.google.com/macros/s/AKfycby7OuvBPd9hZ4OvJmmeydZR1R0XY8t8MUUCRqmmb65A25ab1mojFO_Rkh7vBZcgWb8a7Q/exec"
   },
   staticMaster: {
     addressCsvFilename: "address_master.csv",
     boundariesGeojsonFilename: "boundaries.geojson"
   },
   line: {
-    liffId: ""
+    liffId: "2010941735-IbOXhzpJ"
   },
   features: {
     photoUpload: true,
